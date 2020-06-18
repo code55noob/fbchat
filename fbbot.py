00a90622022f275a7b4bda484fbab69f154fe1f0
code55noob <62043138+code55noob@users.noreply.github.com>
2020-06-18 07:02:43 +0700

    Update fbbot.py

--- fbbot.py
+++ fbbot.py
@@ -4,12 +4,12 @@ from fbchat.models import *
 username = str(input("username : "))
 password = str(input("password : "))
 id_target = int(input("id : "))
-number = int(input("Number of turns : "))
+number = int(input("Number Loop : "))
 msg = str(input("message : "))
 
 client = Client(username,password)
 
-for x in range(number):
+for x in range(number+1):
     print('Own id: {}'.format(client.uid))
     client.send(Message(text=msg), thread_id=id_target, thread_type=ThreadType.USER)
  
